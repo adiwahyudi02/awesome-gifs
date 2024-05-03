43967e1748042c1e45c57710a03dfebb366c3202
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:34:35 +0700

    chore: add nuxt config to handle pinia and layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: [
+    "@nuxt/eslint",
+    "@pinia/nuxt",
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/test-utils/module",
+  ],
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL,
+      apiBaseUrlKey: process.env.API_BASE_URL_KEY,
+    },
+  },
+  components: [
+    // custom scanner for component auto-import
+    { path: "~/components/pages", prefix: "" },
+    { path: "~/components/commons", prefix: "" },
+
+    // It's important that this comes last if you have overrides you wish to apply
+    "~/components",
+  ],
 });
