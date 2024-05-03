757bca54ca93631432c311617525f15d06dc77c1
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:51:46 +0700

    test: create Gif Image compoenent test

---
+++ components/pages/Gif/__test__/Image.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import Image from "../Image.vue";
+
+describe("Image", () => {
+  it("renders properly", () => {
+    const props = {
+      src: "/_ipx/h_50&f_giv&blur_2&q_50/images/image-placeholder.gif",
+    };
+
+    const wrapper = mount(Image, {
+      props,
+    });
+
+    const image = wrapper.find("img");
+    expect(image).toBeDefined();
+    expect(image.attributes().src).toBe(props.src);
+  });
+});
