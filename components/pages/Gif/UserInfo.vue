686435b437385bd7864812c810cc7d5eb00009d5
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:41:23 +0700

    feat: create User Info component

---
+++ components/pages/Gif/UserInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center gap-2">
+    <NuxtImg v-if="avatarSrc" :src="avatarSrc" class="w-10 h-10 rounded-md" />
+    <p class="text-white font-bold text-sm">
+      {{ username }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+type GifUserPropsType = {
+  avatarSrc?: string;
+  username?: string;
+};
+
+withDefaults(defineProps<GifUserPropsType>(), {
+  avatarSrc: "",
+  username: "",
+});
+</script>
