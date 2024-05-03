62665c4fa9f27ef924bc7a951692e6aeea666b0c
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:35:43 +0700

    fix: disable no-expicit-any and multi-word-component-names

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,9 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
+export default withNuxt({
+  rules: {
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "vue/multi-word-component-names": 0,
+  },
+});
