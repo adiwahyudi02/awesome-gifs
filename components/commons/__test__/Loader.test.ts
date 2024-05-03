b38430669440f55e420b19633571a86f3dab2b75
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:50:13 +0700

    test: create Loader compoenent test

---
+++ components/commons/__test__/Loader.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import Loader from "../Loader.vue";
+
+describe("Loader", () => {
+  it("renders properly", () => {
+    const wrapper = mount(Loader);
+    expect(wrapper.vm).toBeTruthy();
+  });
+});
