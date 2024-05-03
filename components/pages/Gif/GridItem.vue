e12dc64d67d6894bcc7b911954cb419d81571633
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:41:56 +0700

    feat: create Gif Grid Item component

---
+++ components/pages/Gif/GridItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :key="item.id" class="relative group cursor-pointer">
+    <GifImage :src="item.images.fixed_height_small.url" />
+
+    <!-- overlay -->
+    <div
+      class="absolute inset-0 bg-transparent opacity-0 group-hover:opacity-100 group-hover:bg-opacity-50 group-hover:bg-black transition-opacity rounded-lg"
+    >
+      <div class="flex flex-col items-start justify-between h-full p-3">
+        <IconForward class="text-white self-end" />
+        <GifUserInfo
+          :avatar-src="item.user?.avatar_url"
+          :username="item.user?.username"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Source } from "~/types/source";
+
+type GridItemPropsType = {
+  item: Source;
+};
+
+defineProps<GridItemPropsType>();
+</script>
