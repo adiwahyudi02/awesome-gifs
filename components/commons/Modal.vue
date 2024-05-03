feeb040ccd88e545b80d9578981c3d3f7f3fad5a
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:37:24 +0700

    feat: create Modal component

---
+++ components/commons/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    v-if="isOpen"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+    @click="$emit('onClose')"
+  >
+    <div class="modal-container">
+      <div class="modal-content p-6" @click.stop>
+        <!-- Close button -->
+        <button
+          class="absolute top-0 right-0 m-4 text-white"
+          @click="$emit('onClose')"
+          data-testid="exit-button"
+        >
+          <IconClose />
+        </button>
+        <!-- Modal content -->
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+type ModalPropsType = {
+  isOpen: boolean;
+};
+
+defineProps<ModalPropsType>();
+
+defineEmits<{
+  (e: "onClose"): void;
+}>();
+</script>
