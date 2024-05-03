d4cbf26ab98c451462b8bea010fb4ac54620638e
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:41:04 +0700

    feat: create Image Gif component

---
+++ components/pages/Gif/Image.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtImg
+    :src="src"
+    class="w-full h-full rounded-lg"
+    :placeholder="
+      img('/images/image-placeholder.gif', {
+        h: 50,
+        f: 'giv',
+        blur: 2,
+        q: 50,
+      })
+    "
+  />
+</template>
+
+<script setup lang="ts">
+type GifImagePropsType = {
+  src: string;
+};
+
+defineProps<GifImagePropsType>();
+
+const img = useImage();
+</script>
