ae1a1b98176ba5c3dd3405c048aedf404f78993a
Adi Wahyudi <adi.whyd02@gmail.com>
2024-05-03 09:42:26 +0700

    feat: create Gif Grid Mansory component

---
+++ components/pages/Gif/Grid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <MasonryWall :items="list" :ssr-columns="0" :column-width="200" :gap="8">
+      <template #default="{ item }">
+        <GifGridItem :item="item" @click="$emit('onSelected', item)" />
+      </template>
+    </MasonryWall>
+    <Loader v-if="isLoading" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Source, SourceList } from "~/types/source";
+
+type GridPropsType = {
+  list: SourceList;
+  isLoading?: boolean;
+};
+
+withDefaults(defineProps<GridPropsType>(), {
+  isLoading: false,
+});
+
+defineEmits<{
+  (event: "onSelected", payload: Source): void;
+}>();
+</script>
